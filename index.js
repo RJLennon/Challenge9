b8d54254058474a39f5a0c21b991716d17756ba3
RJLennon <robjohnlennon@gmail.com>
2023-12-06 22:58:23 -0500

    added inquirer framework

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Question 1?',
+      name: 'Question 1',
+    },
+    {
+      type: 'input',
+      message: 'Question 2?',
+      name: 'Question 2',
+    },
+    {
+      type: 'input',
+      message: 'Question 3?',
+      name: 'Question 3',
+    },
+    {
+      type: 'input',
+      message: 'Question 4?',
+      name: 'Question 4',
+    },
+    {
+      type: 'input',
+      message: 'Question 5?',
+      name: 'Question 5',
+    },
+  ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
