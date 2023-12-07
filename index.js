d87dce110c9a40a4cfea05e4dcdc8083d514a1c2
RJLennon <robjohnlennon@gmail.com>
2023-12-06 23:31:30 -0500

    Minor update

--- index.js
+++ index.js
@@ -7,37 +7,39 @@ const questions = [
     {
         type: 'input',
         message: 'Question 1?',
-        name: 'Question 1',
+        name: 'Question1',
       },
       {
         type: 'input',
         message: 'Question 2?',
-        name: 'Question 2',
+        name: 'Question2',
       },
       {
         type: 'input',
         message: 'Question 3?',
-        name: 'Question 3',
+        name: 'Question3',
       },
       {
         type: 'input',
         message: 'Question 4?',
-        name: 'Question 4',
+        name: 'Question4',
       },
       {
         type: 'input',
         message: 'Question 5?',
-        name: 'Question 5',
+        name: 'Question5',
       },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    //Write data to the file
+};
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions);
-}
+};
 
 // Function call to initialize app
 init();
