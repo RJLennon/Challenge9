3e12a053505f0d94bcee0ce97a154628f4c446a2
RJLennon <robjohnlennon@gmail.com>
2023-12-06 23:05:27 -0500

    Updated to include init function

--- index.js
+++ index.js
@@ -3,42 +3,41 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-inquirer
-  .prompt([
+const questions = [
     {
-      type: 'input',
-      message: 'Question 1?',
-      name: 'Question 1',
-    },
-    {
-      type: 'input',
-      message: 'Question 2?',
-      name: 'Question 2',
-    },
-    {
-      type: 'input',
-      message: 'Question 3?',
-      name: 'Question 3',
-    },
-    {
-      type: 'input',
-      message: 'Question 4?',
-      name: 'Question 4',
-    },
-    {
-      type: 'input',
-      message: 'Question 5?',
-      name: 'Question 5',
-    },
-  ]);
+        type: 'input',
+        message: 'Question 1?',
+        name: 'Question 1',
+      },
+      {
+        type: 'input',
+        message: 'Question 2?',
+        name: 'Question 2',
+      },
+      {
+        type: 'input',
+        message: 'Question 3?',
+        name: 'Question 3',
+      },
+      {
+        type: 'input',
+        message: 'Question 4?',
+        name: 'Question 4',
+      },
+      {
+        type: 'input',
+        message: 'Question 5?',
+        name: 'Question 5',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
